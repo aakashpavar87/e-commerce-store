395831929f63d8d25f685fa910680c40f1cdc342
Aakash Pavar <aakashpavar87@gmail.com>
2024-10-26 18:27:24 +0530

    added backend url in axios client.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://e-commerce-store-1-dbnp.onrender.com/api",
     withCredentials: true, // allow us to send cookies in server.
 });
 
